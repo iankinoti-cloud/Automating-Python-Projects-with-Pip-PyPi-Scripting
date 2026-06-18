e3b20004ba508c59e35adf69bbbf399ae68618b6
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-06-18 06:39:56 +0300

    generate timestamped filename in generate_log

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -3,3 +3,5 @@ from datetime import datetime
 def generate_log(data):
     if not isinstance(data, list):
         raise ValueError("data must be a list")
+
+    filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
